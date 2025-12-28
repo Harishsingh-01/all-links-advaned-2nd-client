12a53e336204ea2898fc74180dc932b842a9f276
harishsingh-01 <harishchaudhary790@gmail.com>
2025-12-28 20:38:58 +0530

    target change

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://all-links-advaned-2nd-server.onrender.com',
         changeOrigin: true,
       },
     },
